d26b972149660fb21d8a537e7174192e7e01dc4a
ZSmaster1 <zaakirsheikh630@gmail.com>
2022-05-07 20:56:33 -0400

    Update leaderboars-sys

--- leaderboard-sys/index.js
+++ leaderboard-sys/index.js
@@ -1,17 +1,72 @@
 require('dotenv').config();
-const { GoogleSpreadsheet } = require('google-spreadsheet');
-const creditials = require('./credentials.json');
+const fs = require('fs');
+const { google } = require('googleapis');
+const auth = new google.auth.GoogleAuth({
+    keyFile: 'credentials.json',
+    scopes: 'https://www.googleapis.com/auth/spreadsheets',
+})
 
-const sheet = new GoogleSpreadsheet(process.env.SHEET_ID);
+const main = async () => {
+    const client = await auth.getClient()
+    const googleSheets = google.sheets({ version: 'v4', auth: client })
+    
+    const getDailyVals = async () => {
+        return (await googleSheets.spreadsheets.values.get({
+            auth,
+            spreadsheetId: process.env.SHEET_ID,
+            range: `Daily!A2:H${process.env.RANGE}`
+        })).data.values?.filter((entry) => entry[0]).map((entry) => {
+            const name = entry[0];
+            const points = entry[entry.length - 1];
+            return { name: name, points: parseInt(points) };
+        })
+    }
 
-(async () => {
-    await sheet.useServiceAccountAuth({
-        client_email: creditials.client_email,
-        private_key: creditials.private_key
-    })
-    await sheet.loadInfo();
-    console.log(`Gained access to "${sheet.title}" sheet`);
+    const getMonthlyVals = async () => {
+        return (await googleSheets.spreadsheets.values.get({
+            auth,
+            spreadsheetId: process.env.SHEET_ID,
+            range: `Monthly!A2:B${process.env.RANGE}`
+        })).data.values?.filter((entry) => entry[0]).map((entry) => {
+            const name = entry[0];
+            const points = entry[entry.length - 1];
+            return { name: name, points: parseInt(points) };
+        })
+    }
 
-    const daily = sheet.sheetsByIndex.filter((n) => n.title.toLowerCase().includes('daily'))[0];
-    const monthly = sheet.sheetsByIndex.filter((n) => n.title.toLowerCase().includes('month'))[0];
-})();
+    const cacheVals = async () => {
+        const values = await getDailyVals();
+
+        values.forEach(async(value) => {
+            const name = value.name.replaceAll(' ', '_');
+            const points = value.points;
+            const date = new Date().toLocaleString().split(',')[0];
+            const fp = `cache/${name}.txt`;
+            if(!fs.existsSync(fp)) fs.writeFileSync(fp, `${date} | ${points}`, { encoding: 'utf-8' });
+            const cur = fs.readFileSync(fp, { encoding: 'utf-8' }) || '';
+            if(cur.includes(date)) return;
+            fs.writeFileSync(fp, `${cur}\n${date} | ${points}`, { encoding: 'utf-8' });
+        })
+    }
+
+    const saveToMonthlySheet = async () => {
+        const daily = await getDailyVals();
+        const monthly = await getMonthlyVals();
+        const combined = daily.map((entry, index) => ([entry.name, entry.points + monthly[index].points ]));
+        console.log(combined);
+        googleSheets.spreadsheets.values.update({
+            auth,
+            spreadsheetId: process.env.SHEET_ID,
+            range: 'Monthly!A:B',
+            valueInputOption: 'USER_ENTERED',
+            resource: {
+                values: [
+                    ['Name', 'Monthly points'],
+                    ...combined
+                ]
+            }
+        }).then(console.log).catch(console.error);
+    }
+}
+
+main();
